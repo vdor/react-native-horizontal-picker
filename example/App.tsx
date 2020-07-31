f11481d37fbdc7390783e826547a75264de25dbb
Vseslav Dorofeev <v.dorofeev@dunice.net>
2020-07-31 21:01:58 +0300

    Added HP

---
+++ example/App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
+import HorizontalPicker from '@vseslav/react-native-horizontal-picker/src';
+
+
+const LN_SIDE_COLOR = '#0B5FA5AA';
+const LN_CENTER_COLOR = '#0B5FA500';
+
+const Items = Array.from(Array(100).keys());
+
+const rednerItem = (item: number, index: number) => (
+  <View style={styles.item}>
+    <Text style={styles.itemText}>
+      {
+        item
+      }
+    </Text>
+  </View>
+)
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <HorizontalPicker
+        data={Items}
+        renderItem={rednerItem}
+        itemWidth={80}
+      />
+      <LinearGradient
+        pointerEvents="none"
+        style={styles.linearGradient}
+        colors={[LN_SIDE_COLOR, LN_CENTER_COLOR, LN_SIDE_COLOR]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0B5FA5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    justifyContent: 'center',
+    width: 80,
+  },
+  itemText: {
+    fontSize: 40,
+    color: "#FF9400",
+    textAlign: 'center',
+  },
+  linearGradient: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
---
+++ src/index.tsx
@@ -0,0 +1,180 @@
+import React, { PureComponent, ReactNode } from 'react';
+import {
+  View,
+  ScrollView,
+  ScrollViewProps,
+  TouchableWithoutFeedback,
+  LayoutChangeEvent,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+  Platform,
+} from 'react-native';
+
+
+export interface HorizontalPickerProps extends ScrollViewProps {
+  data: any[],
+  renderItem: (item: any, index: number) => ReactNode,
+  itemWidth: number,
+  snapTimeout?: number
+  onChange?: (position: number) => void
+};
+
+
+export type HorizontalPickerState = {
+  scrollViewWidth: number
+};
+
+
+
+export default class HorizontalPicker extends PureComponent<HorizontalPickerProps, HorizontalPickerState> {
+  private paddingSide: number;
+  private refScrollView: React.RefObject<ScrollView>;
+  private ignoreNextScroll: boolean;
+  private timeoutDelayedSnap: number | NodeJS.Timeout;
+  private currentPositionX: number;
+  private readonly defaultScrollEventThrottle = 16;
+  private readonly defaultDecelerationRate = Platform.OS == "ios" ? 50 : 0.9;
+  private readonly defaultSnapTimeout = 500;
+
+  constructor(props: HorizontalPickerProps) {
+    super(props);
+    this.paddingSide = 0;
+    this.refScrollView = React.createRef();
+    this.ignoreNextScroll = false;
+    this.timeoutDelayedSnap = 0;
+    this.currentPositionX = 0;
+
+    this.state = {
+      scrollViewWidth: 0,
+    };
+  }
+
+  private onLayoutScrollView = (e: LayoutChangeEvent) => {
+    const { width } = e.nativeEvent.layout;
+    this.setState(() => ({ scrollViewWidth: width }));
+    this.paddingSide = width / 2 - this.props.itemWidth / 2;
+
+    if (this.props.onLayout != null) {
+      this.props.onLayout(e);
+    }
+  }
+
+  private onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    this.currentPositionX = e.nativeEvent.contentOffset.x;
+
+    if (this.props.onScroll != null) {
+      this.props.onScroll(e);
+    }
+  }
+
+  private onScrollBeginDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    this.ignoreNextScroll = false;
+
+    if (this.props.onScrollBeginDrag != null) {
+      this.props.onScrollBeginDrag(e);
+    }
+  }
+
+  private onScrollEndDrag = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    this.cancelDelayedSnap();
+    if (this.ignoreNextScroll) {
+      this.ignoreNextScroll = false;
+    } else {
+      this.setDelayedSnap();
+    }
+
+    if (this.props.onScrollEndDrag != null) {
+      this.props.onScrollEndDrag(e);
+    }
+  }
+
+  private onMomentumScrollBegin = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    this.ignoreNextScroll = false;
+    this.setDelayedSnap();
+
+    if (this.props.onMomentumScrollBegin != null) {
+      this.props.onMomentumScrollBegin(e);
+    }
+  }
+
+  private onMomentumScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (this.ignoreNextScroll) {
+      this.ignoreNextScroll = false;
+    } else {
+      this.setDelayedSnap();
+    }
+
+    if (this.props.onMomentumScrollEnd != null) {
+      this.props.onMomentumScrollEnd(e);
+    }
+  }
+
+  private scrollToPosition = (position: number) => {
+    const { itemWidth } = this.props;
+    const x = position * itemWidth;
+    this.ignoreNextScroll = true;
+
+    if (this.refScrollView.current != null) {
+      this.refScrollView.current.scrollTo({ x, y: 0, animated: true });
+    }
+  }
+
+  private cancelDelayedSnap = () => {
+    clearTimeout(this.timeoutDelayedSnap as NodeJS.Timeout);
+  }
+
+  private setDelayedSnap = (timeout?: number) => {
+    const snapTimeout = timeout || this.props.snapTimeout || this.defaultSnapTimeout;
+    const {
+      itemWidth,
+      onChange,
+    } = this.props;
+    this.cancelDelayedSnap();
+    this.timeoutDelayedSnap = setTimeout(() => {
+      const nextPosition = Math.round(this.currentPositionX / itemWidth);
+      this.scrollToPosition(nextPosition);
+
+      if (onChange != null) {
+        onChange(nextPosition);
+      }
+    }, snapTimeout);
+  }
+
+  render() {
+    const {
+      data,
+      renderItem,
+      ...props
+    } = this.props;
+
+    return (
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={this.defaultScrollEventThrottle}
+        decelerationRate={this.defaultDecelerationRate}
+        contentContainerStyle={{ paddingHorizontal: this.paddingSide }}
+        ref={this.refScrollView}
+        onLayout={this.onLayoutScrollView}
+        onScroll={this.onScroll}
+        onScrollBeginDrag={this.onScrollBeginDrag}
+        onScrollEndDrag={this.onScrollEndDrag}
+        onMomentumScrollBegin={this.onMomentumScrollBegin}
+        onMomentumScrollEnd={this.onMomentumScrollEnd}
+        {...props}
+      >
+        {
+          data.map((item: any, index: number) => (
+            <TouchableWithoutFeedback onPress={() => this.scrollToPosition(index)} key={index}>
+              <View>
+                {
+                  renderItem(item, index)
+                }
+              </View>
+            </TouchableWithoutFeedback>
+          ))
+        }
+      </ScrollView>
+    );
+  }
+}
